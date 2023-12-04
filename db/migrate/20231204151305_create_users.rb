08abf03a85704a049873816f5a284d37d677ff82
Ricardo Martinez <ricardo.martinez.montero@gmail.com>
2023-12-04 18:45:05 +0000

    Add yarn missing dependencies

--- db/migrate/20231204151305_create_users.rb
+++ db/migrate/20231204151305_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password
+      t.string :password_digest
+      t.string :password_confirmation
       t.string :icon
       t.string :introduction
 
