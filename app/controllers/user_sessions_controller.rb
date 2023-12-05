85328a08b4b04b91117804772151206cc0bc1f11
Ricardo Martinez <ricardo.martinez.montero@electroroute.co.jp>
2023-12-05 14:07:55 +0000

    Add debugger and user links update

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,22 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:alert] = "Login failed"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'pages/secret'
+
+  get '/login', to: 'user_sessions#new'
+  post '/sessions', to: 'user_sessions#create'
+  delete '/sessions', to: 'user_sessions#destroy'
+
+  resources :users, only: [:index, :new, :create]
 end
