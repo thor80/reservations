d40904ca62f6b53854e85dd1afe641ec15c3bc93
Ricardo Martinez <ricardo.martinez.montero@electroroute.co.jp>
2023-12-06 13:08:56 +0000

    Add bootstrap and user profile management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,70 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "User created successfully"
-      redirect_to users_path
+      redirect_to root_path
     else
       flash[:alert] = "User not created"
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit_password
+    if current_user.blank?
+      redirect root_path
+    end
+    @user = current_user
+  end
+
+  def update_password
+    if current_user.blank?
+      redirect root_path
+    end
+    @user = current_user
+    if @user.update(user_password_params)
+      redirect_to root_path, notice: 'Password was successfully updated.'
+    else
+      flash[:alert] = "Password hasn't been updated"
+      render :edit_password, status: :unprocessable_entity
+    end
+  end
+
+  def edit_profile
+    if current_user.blank?
+      redirect root_path
+    end
+    @user = current_user
+  end
+
+  def update_profile
+    require 'fileutils'
+    if current_user.blank?
+      redirect root_path
+    end
+
+    @user = current_user
+
+    @user.name = user_profile_params[:name]
+    @user.introduction = user_profile_params[:introduction]
+
+    icon_io = user_profile_params[:icon]
+    icon_path = nil
+    if icon_io
+      icon_io.original_filename
+      icon_path = Rails.root.join('public', 'uploads', @user.id.to_s, icon_io.original_filename)
+      icon_path_resource = 'uploads/' + @user.id.to_s + "/" + icon_io.original_filename
+    end
+
+    if @user.update(name: user_profile_params[:name], introduction: user_profile_params[:introduction], icon: icon_path_resource)
+      if icon_path
+        FileUtils.mkdir_p Rails.root.join('public', 'uploads', @user.id.to_s)
+        File.open(icon_path, 'wb') do |file|
+          file.write(icon_io.read)
+        end
+      end
+      redirect_to root_path, notice: 'Profile was successfully updated.'
+    else
+      flash[:alert] = "Profile hasn't been updated"
+      render :edit_profile, status: :unprocessable_entity
     end
   end
 
@@ -23,6 +83,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def user_password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def user_profile_params
+    params.require(:user).permit(:name, :introduction, :icon)
   end
 
 end
