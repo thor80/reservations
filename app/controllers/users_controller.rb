8ae93109eaac24edd5857f1690cf18434a71b0de
Ricardo Martinez <ricardo.martinez.montero@gmail.com>
2023-12-06 14:26:05 +0000

    Add comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+
+  # This list all users, but is not linked to any view at this time
   def index
     @users = User.all
   end
 
+  # Prepare new user form
   def new
     @user = User.new
   end
 
+  # Create new user and go to index
   def create
     @user = User.new(user_params)
 
@@ -19,6 +23,7 @@ class UsersController < ApplicationController
     end
   end
 
+  # Prepare view for user password edit form
   def edit_password
     if current_user.blank?
       redirect root_path
@@ -26,6 +31,7 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  # Update user password
   def update_password
     if current_user.blank?
       redirect root_path
@@ -39,6 +45,7 @@ class UsersController < ApplicationController
     end
   end
 
+  # Init user profile form
   def edit_profile
     if current_user.blank?
       redirect root_path
@@ -46,6 +53,7 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  # Update profile for user
   def update_profile
     require 'fileutils'
     if current_user.blank?
@@ -79,16 +87,20 @@ class UsersController < ApplicationController
     end
   end
 
+  # Here methods to extract parameters that will be used in different forms
   private
 
+  # To create new user we need this parameters
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  # To change password we need this parameters
   def user_password_params
     params.require(:user).permit(:password, :password_confirmation)
   end
 
+  # To change user profile we need this parameters
   def user_profile_params
     params.require(:user).permit(:name, :introduction, :icon)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # this is the default content that is shown when you access to http://localhost:3000
   root 'pages#index'
 
+  # Here are the roots to access login page.
   get '/login', to: 'user_sessions#new'
+  # Here is the root when you login, and create session for user
   post '/sessions', to: 'user_sessions#create'
+  # This is to remove session for user and log him out
   delete '/sessions', to: 'user_sessions#destroy'
 
-
+  # Here is the link to the page where the user edit his password
   get '/edit_password', to: 'users#edit_password'
+  # Here is the link to save the new password and go back to root the index page.
   patch '/edit_password', to: 'users#update_password'
 
+  # Here is the link to send the user to the profile edition page
   get '/edit_profile', to: 'users#edit_profile'
+  # Here is the link to update the user profile and send him to index page.
   patch '/edit_profile', to: 'users#update_profile'
 
-  resources :users, only: [:index, :new, :create]
+  # Here are the links to go to new user form, and to create user.
+  get '/users/new', to: 'users#new'
+  post '/users/new', to: 'users#create'
+
 end
